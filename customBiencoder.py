16cbb8b889a10361e5a64c4724c397e7ecfe1007
aidan073 <aidanbell073@gmail.com>
2024-10-28 17:03:31 -0400

    added batch encoding, began writeTopN method (incomplete), accommodation changes

--- customBiencoder.py
+++ customBiencoder.py
@@ -1,5 +1,8 @@
-from sentence_transformers import SentenceTransformer
+import numpy as np
 import torch
+import csv
+from sentence_transformers import SentenceTransformer
+from sklearn.metrics.pairwise import cosine_similarity
 
 class CustomBiencoder():
     def __init__(self, model_name:str) -> None:
@@ -10,3 +13,14 @@ class CustomBiencoder():
         return self.model
     def setModel(self, model) -> None:
         self.model = model
+    def getEmbeddings(self, batch):
+        embeddings = self.model.encode(batch, batch_size=32, show_progress_bar=True)
+        return embeddings
+    def writeTopN(queries, collection, top_n, q_map, d_map, output_file):
+        similarities = cosine_similarity(queries, collection)
+        query_results = []
+        with open(output_file, "w", newline = '') as f:
+            writer = csv.writer(f)
+            for i in len(queries):
+                top_indices = np.argsort(similarities[i])[::-1][:top_n]
+                query_results.append([(q_map[i], similarities[i][j]) for j in top_indices])
