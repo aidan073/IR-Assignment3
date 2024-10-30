81379c1ff850e1b362fae7a8c9265e269a9be251
“Evan <evan.koenig@maine.edu>
2024-10-29 20:34:22 -0400

    Started finetuning step of crossencoder

--- customCrossencoder.py
+++ customCrossencoder.py
@@ -1,4 +1,9 @@
-from sentence_transformers import CrossEncoder, util
+from sentence_transformers import InputExample, CrossEncoder, util
+from sentence_transformers.evaluation import BinaryClassificationEvaluator, CESoftmaxAccuracyEvaluator
+
+import torch
+import csv
+from torch.utils.data import DataLoader
 import torch
 
 class CustomCrossencoder():
@@ -12,3 +17,36 @@ class CustomCrossencoder():
         self.model = model
     def cosineSim(self, emb1, emb2) -> torch.Tensor:
         return util.cos_sim(emb1, emb2)
+    def rerank(self, dict, topics, collection) -> None:
+        with open('result_ce_1.tsv', 'w', newline='') as cetsv:
+            ce_writer = csv.writer(cetsv, delimiter='\t')
+            for q_id, answers in dict.items():
+                unranked_dict = {}
+                for a_id in answers:
+                    score = self.model.predict([(topics[q_id], collection[a_id])])
+                    unranked_dict[a_id] = score[0]
+                ranked_dict = dict(sorted(unranked_dict.items(), key=lambda item: item[1], reverse=True))
+                rank = 1
+                for key, value in ranked_dict.items():
+                    ce_writer.writerow([q_id, 'Q0', key, rank, value, "ce1"])
+                    rank += 1
+    def fine_tune(self, train_data, val_data, test_data, epochs=1, batch_size=16, warmup_steps=100):
+        train_examples = [InputExample(texts=[ex[0], ex[1]], label=ex[2]) for ex in train_data]
+        val_examples = [InputExample(texts=[ex[0], ex[1]], label=ex[2]) for ex in val_data]
+        test_examples = [InputExample(texts=[ex[0], ex[1]], label=ex[2]) for ex in test_data]
+        
+        train_dataloader = DataLoader(train_examples, shuffle=True, batch_size=batch_size)
+        evaluator = CESoftmaxAccuracyEvaluator.from_input_examples(val_examples, name="validation")
+
+        self.model.fit(
+            train_dataloader=train_dataloader,
+            evaluator=evaluator,
+            epochs=epochs,
+            evaluation_steps=1000,
+            warmup_steps=warmup_steps,
+            output_path="fine_tuned_crossencoder"
+        )
+
+        evaluator = BinaryClassificationEvaluator.from_input_examples(val_examples, name="validation")
+        test_evaluator = BinaryClassificationEvaluator.from_input_examples(test_examples, name="test")
+
--- main.py
+++ main.py
@@ -44,15 +44,4 @@ crossencoder = CustomCrossencoder("cross-encoder/ms-marco-MiniLM-L-6-v2")
 crossmodel = crossencoder.getModel()
 testdict = data.readTSV('data/result_bm25_1.tsv')
 
-with open('result_ce_1.tsv', 'w', newline='') as cetsv:
-    ce_writer = csv.writer(cetsv, delimiter='\t')
-    for q_id, answers in testdict.items():
-        unranked_dict = {}
-        for a_id in answers:
-            score = crossmodel.predict([(topics[q_id], collection[a_id])])
-            unranked_dict[a_id] = score[0]
-        ranked_dict = dict(sorted(unranked_dict.items(), key=lambda item: item[1], reverse=True))
-        rank = 1
-        for key, value in ranked_dict.items():
-            ce_writer.writerow([q_id, 'Q0', key, rank, value, "ce1"])
-            rank += 1
+#crossmodel.rerank(testdict, topics, collection)
