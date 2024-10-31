48811466eee81e034f285240ff8d17c69aff246f
aidan073 <aidanbell073@gmail.com>
2024-10-31 19:43:37 -0400

    "Parse args added"

--- main.py
+++ main.py
@@ -4,53 +4,76 @@ from customBiencoder import CustomBiencoder
 import argparse
 import csv
 
-#parser = argparse.ArgumentParser(description="Process paths for topics, collections, qrels, and answers.")
+parser = argparse.ArgumentParser(description="Process paths for topics, collection, fine-tuned model paths, result file output paths, optional qrel and embeddings.")
 
 # positional arguments
-# parser.add_argument("topic_path", type=str, help="Path to the topic file")
-# parser.add_argument("collection_path", type=str, help="Path to the collection file")
-# parser.add_argument("-f", "--qrel_path", type=str, help="Path to the qrel file")
-# parser.add_argument("-L", "--load_paths", nargs=2, metavar=("TOPICS_LOAD_PATH", "ANSWERS_LOAD_PATH"),
-#                     help="Paths containing pre-encoded topic/answer embeddings")
+parser.add_argument("topic_path", type=str, help="Path to the topic file")
+parser.add_argument("collection_path", type=str, help="Path to the collection file")
+parser.add_argument("-f", "--fine_tuned_paths", nargs=2, metavar=("BI_ENCODER_FT_PATH", "CE_FT_PATH"),help="Path to fine-tuned models")
+parser.add_argument("-o", "--output_paths", nargs=4, required=True, metavar=("BI_OUTPUT_PATH", "BI_OUTPUT_PATH_FT", "CE_OUTPUT_PATH", "CE_OUTPUT_PATH_FT"),help="Desired output paths")  
+parser.add_argument("-q", "--qrel_path", type=str, help="Path to qrel file")
+parser.add_argument("-L", "--load_paths", nargs=2, metavar=("TOPIC_EMBEDDINGS_PATH", "ANSWER_EMBEDDINGS_PATH"),help="Paths containing pre-encoded topic/answer embeddings for bi-encoder")
 
-# arguments = parser.parse_args()
+args = parser.parse_args()
+
+topic_path = args.topic_path
+collection_path = args.collection_path
+bi_output_path, bi_output_path_ft, ce_output_path, ce_output_path_ft = args.output_paths
+fine_tuned_paths = None
+qrel_path = None
+load_paths = None 
+if args.fine_tuned_paths:
+    ft_bi_path, ft_ce_path = args.fine_tuned_paths
+if args.qrel_path:
+    qrel_path = args.qrel_path
+if args.load_paths:
+    topic_emb_path, collection_emb_path = args.load_paths
 
 # data processing
-data = DataProcessor("data/topics_1.json", "data/Answers.json", "data/qrel_1.tsv")
-#data = DataProcessor("data/topics_1_mini.json", "data/Answers_mini.json", "data/qrel_1_mini")
-topics, topic_batch, topic_map, t_Rmap = data.getTopics(get_batch=True, get_map=True, get_Rmap=True)
-collection, collection_batch, collection_map, d_Rmap = data.getCollection(get_batch=True, get_map=True, get_Rmap=True)
+data = DataProcessor(topic_path, collection_path, qrel_path)
+topics, topic_batch, topic_map = data.getTopics(get_batch=True, get_map=True)
+collection, collection_batch, collection_map = data.getCollection(get_batch=True, get_map=True)
+print("Data processing complete\n")
 
 # bi-encoder model setup
 encoder = CustomBiencoder("sentence-transformers/multi-qa-MiniLM-L6-cos-v1")
 model = encoder.getModel()
 
-# run
-#outfile_name = "results/topics2"
-#topic_embeddings = encoder.getEmbeddings(topic_batch, outfile_name)
-#outfile_name = "results/collection"
-#collection_embeddings = encoder.getEmbeddings(collection_batch, outfile_name)
-#topic_embeddings = encoder.getEmbeddings(topic_batch)
-#collection_embeddings = encoder.getEmbeddings(collection_batch)
+# run base bi-encoder
+if load_paths:
+    topic_embeddings = encoder.loadEmbeddings(topic_emb_path)
+    collection_embeddings = encoder.loadEmbeddings(collection_emb_path)
+else:
+    topic_embeddings = encoder.getEmbeddings(topic_batch)
+    collection_embeddings = encoder.getEmbeddings(collection_batch)
+encoder.writeTopN(topic_embeddings, collection_embeddings, "bi_encoder", bi_output_path, q_map=topic_map, d_map=collection_map)
 
-topic_embeddings = encoder.loadEmbeddings("results/topics.npy")
-collection_embeddings = encoder.loadEmbeddings("results/collection.npy")
-# encoder.writeTopN(topic_embeddings, collection_embeddings, "bi_encoder", "results/result_bi_1.tsv", q_map=topic_map, d_map=collection_map)
+print("Bi-encoder base results obtained\n")
 
 # fine-tuning
-qrel = data.getQrel()
-samples = data.formatSamples(topics, collection, qrel)
-train_data, test_data, val_data = data.test_train_split(samples)
-encoder.fineTune(train_data, val_data, 3)
-model.save("data/modelF")
-#encoder.loadModel("data/modelF")
+if qrel_path:
+    qrel = data.getQrel()
+    samples = data.formatSamples(topics, collection, qrel)
+    train_data, test_data, val_data = data.test_train_split(samples)
+    if not fine_tuned_paths:
+        encoder.fineTune(train_data, val_data, 3)
+        model.save("model/fine-tuned-be")
+        print("Fine-tuning complete\n")
+    else:
+        encoder.loadModel(ft_bi_path)
+    test_topic_batch, test_collection_batch, t_qmap, t_dmap = data.getSubsetBatches(test_data)
+    test_topic_embs = encoder.getEmbeddings(test_topic_batch)
+    test_collection_embs = encoder.getEmbeddings(test_collection_batch)
+    data.genQrel(test_data)
+    encoder.writeTopN(test_topic_embs, test_collection_embs, "bi_encoder_fine-tuned", bi_output_path_ft, q_map=t_qmap, d_map=t_dmap)
+    print("Bi-encoder fine-tuned results obtained\n")
+else:
+    encoder.loadModel(ft_bi_path)
+    topic_embeddings = encoder.getEmbeddings(topic_batch)
+    collection_embeddings = encoder.getEmbeddings(collection_batch)
+    encoder.writeTopN(topic_embeddings, collection_embeddings, "bi_encoder_fine-tuned", bi_output_path_ft, q_map=topic_map, d_map=collection_map)
+    print("Bi-encoder fine-tuned results obtained\n")
 
-test_topic_batch, test_collection_batch, t_qmap, t_dmap = data.getSubsetBatches(test_data)
-test_topic_embs = encoder.getEmbeddings(test_topic_batch)
-test_collection_embs = encoder.getEmbeddings(test_collection_batch)
-data.genQrel(test_data)
-
-encoder.writeTopN(test_topic_embs, test_collection_embs, "bi_encoder_fine-tuned", "results/result_bi_1.tsv", q_map = t_qmap, d_map=t_dmap)
 
 # cross-encoder model setup
 # crossencoder = CustomCrossencoder("cross-encoder/ms-marco-MiniLM-L-6-v2")
